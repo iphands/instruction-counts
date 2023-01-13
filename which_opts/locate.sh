818f6701749021e86582ebe4cc9e837832a1af39
Ian Page Hands <iphands@gmail.com>
2023-01-12 20:28:14 -0800

    add osx

--- which_opts/locate.sh
+++ which_opts/locate.sh
@@ -6,9 +6,25 @@ add() {
   echo "$1" >> "$BIN_LIST"
 }
 
-rm "$BIN_LIST" || true
+do_linux() {
+  for var in `find /usr/bin -type f`
+  do
+    file "$var" | grep -q 'ELF.*executable' && add "$var" || true
+  done
+}
 
-for var in `find /usr/bin -type f`
-do
-  file "$var" | grep -q 'ELF.*executable' && add "$var" || true
-done
+do_darwin() {
+  for var in `find /usr/bin -type f`
+  do
+    file "$var" | grep -qm1 'universal binary' && add "$var" || true
+  done
+}
+
+rm "$BIN_LIST" || true
+if [[ "$OSTYPE" == "darwin"* ]]
+then
+  do_darwin
+  exit 0
+fi
+
+do_linux
--- which_opts/make_counts.sh
+++ which_opts/make_counts.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 source ./env
-cat $BIN_LIST | parallel --jobs 30 bash ./worker.sh
+cat $BIN_LIST | parallel --jobs 4 bash ./worker.sh
